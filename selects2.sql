159a0be4e50771a7d2cb023b759b4b98a2b76522
Kamil <sqiche@mail.ru>
2025-06-30 20:38:06 +0300

    redact code file selects2.sql...

--- selects2.sql
+++ selects2.sql
@@ -13,10 +13,13 @@ FROM Track t
 JOIN Album a ON t.albumID = a.albumID
 GROUP BY a.name;
 
-select e.name from Album a
-join Album_Executor ae on a.albumID = ae.albumID
-join Executor e on e.executorID = ae.executorID
-where year_release < 2020 or year_release > 2020 ;
+with T as (
+	select e.name from Album a
+	join Album_Executor ae on a.albumID = ae.albumID
+	join Executor e on e.executorID = ae.executorID
+	where year_release < 2020 or year_release > 2020 
+)
+select count(*) from T;
 
 select DISTINCT c.name from Collection c
 join collection_track ct on c.collectionid = ct.collectionid
@@ -25,3 +28,13 @@ join album a on a.albumid = t.albumid
 join album_executor ae on ae.albumid = a.albumid
 join executor e on e.executorid = ae.executorid
 where e.name = 'Eminem';
+
+SELECT 
+    e.name AS executor_name,
+    COUNT(DISTINCT a.albumID) AS album_count 
+FROM Executor e
+JOIN Album_Executor ae ON e.executorID = ae.executorID
+JOIN Album a ON ae.albumID = a.albumID
+WHERE a.year_release <> 2020 
+GROUP BY e.name
+ORDER BY album_count DESC;
